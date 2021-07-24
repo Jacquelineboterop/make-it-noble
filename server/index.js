84aa818399a904d31c6dd51215febc4eab26fe99
Jacqueline <proyectos@branta.com.co>
2021-07-24 16:55:09 -0500

    register review

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-
 const mongoose = require("mongoose");
 const { response } = require("express");
 const cookieSession = require("cookie-session");
@@ -9,21 +8,17 @@ const cookieSession = require("cookie-session");
 const app = express();
 const PORT = 3000;
 
-
 app.use(cookieSession({
-secret: " ",
-maxAge: 5 * 60 * 1000,
+  secret: " ",
+  maxAge: 5 * 60 * 1000,
 }));
 //Mongo
 
 mongoose.connect("mongodb://localhost:27017/noble", {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 mongoose.connection.on("error", (error) => console.error(error));
 mongoose.connection.once("open", () => console.log("Mongoose connected"));
-
-
-
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -41,36 +36,47 @@ app.post("/login", async (request, response) => {
     const { userName, password } = request.body;
     const user = await UserModel.findOne({ userName }).exec();
     if (user) {
-      console.log(userName)
       const passwordMatch = await bcrypt.compare(password, user.password);
       if (passwordMatch) {
         response.status(200).json({
-        logged: true,
-        })
-        response.redirect("/");
-
-      }
-    }
-    response.status(200).json({
-      logged: false,
-    })
+          logged: true,
+        });
+      } else {
+        response.status(200).json({
+          logged: false,
+        });
+      }  
+    } else {
+        response.status(200).json({
+          logged: false,
+      });
+    }  
   } catch (error) {
-    console.log(error)
     response.status(400).json(error)
   }
-})
+});
+
 //Register
 app.post("/register", async (request, response) => {
   try {
     const { password, ...data } = request.body;
     const encryptedPassword = await bcrypt.hash(password, 10);
-    const user = await UserModel.create({ ...data, password: encryptedPassword });
-    response.status(200).json({
-      user
-    })
+
+    if (data.name=="" || data.lastNames=="" || password=="" || data.userName=="") {
+      response.status(400).json({
+        created: false,
+      });
+      
+    } else {
+      const user = await UserModel.create({ ...data, password: encryptedPassword });
+      response.status(200).json({
+        user,
+        create:true,
+      });
+    }  
   } catch (error) {
-    console.log(error);
-    response.status(500).json(error);
+    response.status(400).json(error);
   }
 });
+
 app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
