35508c7cad933bd5c94a00e753fea0751e3889e4
Jacqueline <proyectos@branta.com.co>
2021-07-25 07:57:02 -0500

    logout fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-
 const mongoose = require("mongoose");
 const { response } = require("express");
 const cookieSession = require("cookie-session");
@@ -9,26 +8,24 @@ const cookieSession = require("cookie-session");
 const app = express();
 const PORT = 3000;
 
-
+app.use(cookieSession({
+  secret: " ",
+  maxAge: 5 * 60 * 1000,
+}));
 
 //Mongo
-
 mongoose.connect("mongodb://localhost:27017/noble", {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 mongoose.connection.on("error", (error) => console.error(error));
 mongoose.connection.once("open", () => console.log("Mongoose connected"));
 
-
-
-
 const UserSchema = new mongoose.Schema({
   name: String,
   lastname: String,
-  userName: String,
+  username: String,
   password: String,
 });
-
 
 const PostSchema = new mongoose.Schema({
   noteIn: String,
@@ -43,27 +40,35 @@ app.use(express.json());
 app.use(cookieSession({
   secret: " ",
   maxAge: 5 * 60 * 1000,
-  }));
+}));
+
 //Register
 app.post("/register", async (request, response) => {
   try {
     const { password, ...data } = request.body;
     const encryptedPassword = await bcrypt.hash(password, 10);
-    const user = await UserModel.create({ ...data, password: encryptedPassword });
-    response.status(200).json({
-      user
-    })
+
+    if (data.name=="" || data.lastname=="" || password=="" || data.username=="") {
+      response.status(400).json({
+        created: false,
+      });
+      
+    } else {
+      const user = await UserModel.create({ ...data, password: encryptedPassword });
+      response.status(200).json({
+        create:true,
+      });
+    }  
   } catch (error) {
-      console.log(error);
-      response.status(500).json(error);
-    }
+    response.status(400).json(error);
+  }
 });
 
 //Login
 app.post("/login", async (request, response) => {
   try {
-    const { userName, password } = request.body;
-    const user = await UserModel.findOne({ userName }).exec();
+    const { username, password } = request.body;
+    const user = await UserModel.findOne({ username }).exec();
     if (user) {
       const passwordMatch = await bcrypt.compare(password, user.password);
       if (passwordMatch) {
@@ -72,18 +77,17 @@ app.post("/login", async (request, response) => {
           logged: true,
         })
       } else {
-        response.status(200).json({
+        response.status(401).json({
           logged: false,
         })
       }
     } else {
-      response.status(200).json({
+      response.status(401).json({
         logged: false,
         })
     } 
   } catch (error) {
-    console.log(error);
-    response.status(400).json(error);
+    response.status(401).json(error);
   }
 })
 
@@ -92,43 +96,50 @@ app.post("/post", async (request, response) => {
   const user =  request.session.userId;
   try {
     const { userUuid, ...data } = request.body;
-    console.log(user);
     if (user) {
       const post = await PostModel.create({ ...data, userUuid: user});
       response.status(200).json({
         created: true,
-      })
-    }
+      });
+    } else {
+      response.status(401).json(error);
+    }  
   } catch (error) {
-    console.log(error);
-    response.status(401).json(error);
+    response.status(500).json(error);
   }
-})
+});
 
 //Posts
 app.post("/posts", async (request, response) => {
   const user =  request.session.userId;
   try {
     const { userUuid, ...data } = request.body;
-    console.log(user);
     if (user) {
       const post = await PostModel.find({userUuid: user});
       response.status(200).json({
         publicaciones: post,
-      })
-    }
+      });
+    } else {
+      response.status(401).json(error);
+    }  
   } catch (error) {
-    console.log(error);
     response.status(500).json(error);
   }
-})
+});
+
 //Logout
 app.get("/logout", (request, response) => {
-  request.session.userId = null;
-  response.status(200).json({
-    loggedOut: true,
-  })
-  
+  const user =  request.session.userId;
+  if (user) {
+    request.session.userId = null;
+    response.status(200).json({
+      loggedOut: true,
+    }); 
+  } else {
+    response.status(400).json({
+      error: "bad request",
+    });  
+  } 
 });
 
 app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
