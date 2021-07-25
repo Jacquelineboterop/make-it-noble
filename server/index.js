bce09d1e5ffb7900dd28b3089d8085d4c0982ad5
Jacqueline <proyectos@branta.com.co>
2021-07-25 07:18:02 -0500

    login fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-
 const mongoose = require("mongoose");
 const { response } = require("express");
 const cookieSession = require("cookie-session");
@@ -9,27 +8,23 @@ const cookieSession = require("cookie-session");
 const app = express();
 const PORT = 3000;
 
-
 app.use(cookieSession({
-secret: " ",
-maxAge: 5 * 60 * 1000,
+  secret: " ",
+  maxAge: 5 * 60 * 1000,
 }));
 //Mongo
 
 mongoose.connect("mongodb://localhost:27017/noble", {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 mongoose.connection.on("error", (error) => console.error(error));
 mongoose.connection.once("open", () => console.log("Mongoose connected"));
 
-
-
-
 const UserSchema = new mongoose.Schema({
   name: String,
   lastname: String,
-  userName: String,
-  password: String,
+  username: String,
+  password: { type: String, select: false },
 });
 
 const UserModel = mongoose.model("User", UserSchema);
@@ -38,38 +33,49 @@ app.use(express.json());
 //Cookie
 app.post("/login", async (request, response) => {
   try {
-    const { userName, password } = request.body;
-    const user = await UserModel.findOne({ userName }).exec();
+    const { username, password } = request.body;
+    const user = await UserModel.findOne({ username }).exec();
     if (user) {
-      console.log(userName)
       const passwordMatch = await bcrypt.compare(password, user.password);
       if (passwordMatch) {
         response.status(200).json({
           logged: true,
-        })
-        response.redirect("/");
-      }
-    }
-    response.status(200).json({
-      logged: false,
-    })
+        });
+      } else {
+        response.status(200).json({
+          logged: false,
+        });
+      }  
+    } else {
+      response.status(200).json({
+        logged: false,
+      });
+    }  
   } catch (error) {
-    console.log(error);
     response.status(400).json(error);
   }
-})
+});
+
 //Register
 app.post("/register", async (request, response) => {
   try {
     const { password, ...data } = request.body;
     const encryptedPassword = await bcrypt.hash(password, 10);
-    const user = await UserModel.create({ ...data, password: encryptedPassword });
-    response.status(200).json({
-      user
-    })
+  
+    if (data.name=="" || data.lastname=="" || password=="" || data.username=="") {
+      response.status(400).json({
+        created: false,
+      });
+      
+    } else {
+      const user = await UserModel.create({ ...data, password: encryptedPassword });
+      response.status(200).json({
+        create:true,
+      });
+    }  
   } catch (error) {
-    console.log(error);
-    response.status(500).json(error);
+    response.status(400).json(error);
   }
 });
+
 app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
